98294418333a1eafc92049175f8221b39d3177b7
laole918 <laole918@qq.com>
2016-03-27 15:29:36 +0800

    修复光标在部分机型上面不显示

--- app/src/main/java/com/aspsine/edittextinlistview/LineAdapter.java
+++ app/src/main/java/com/aspsine/edittextinlistview/LineAdapter.java
@@ -52,6 +52,18 @@ public class LineAdapter extends BaseAdapter {
 
         final Line line = lines.get(position);
 
+        holder.etLine.setCursorVisible(false);//确保只有获取焦点的EditText才能显示光标
+        holder.etLine.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    holder.etLine.setCursorVisible(true);
+                } else {
+                    holder.etLine.setCursorVisible(false);
+                }
+            }
+        });
+
         if (holder.etLine.getTag() instanceof TextWatcher) {
             holder.etLine.removeTextChangedListener((TextWatcher) (holder.etLine.getTag()));
         }
@@ -70,13 +82,10 @@ public class LineAdapter extends BaseAdapter {
             }
             CharSequence text = line.getText();
             holder.etLine.setSelection(TextUtils.isEmpty(text) ? 0 : text.length());
-            // 使光标闪烁
-            holder.etLine.setCursorVisible(true);
         } else {
             if (holder.etLine.isFocused()) {
                 holder.etLine.clearFocus();
             }
-            holder.etLine.setCursorVisible(false);
         }
 
         holder.etLine.setOnTouchListener(new View.OnTouchListener() {
@@ -87,7 +96,7 @@ public class LineAdapter extends BaseAdapter {
                     check(position);
                     if (!focus && !holder.etLine.isFocused()) {
                         holder.etLine.requestFocus();
-                        holder.etLine.onWindowFocusChanged(true);
+//                        holder.etLine.onWindowFocusChanged(true);
                     }
                 }
                 return false;
@@ -108,21 +117,6 @@ public class LineAdapter extends BaseAdapter {
         holder.etLine.addTextChangedListener(watcher);
         holder.etLine.setTag(watcher);
 
-        // 第6步：设置OnFocusChangeListener当EditText获取焦点后重新设置光标visible为true。
-        //
-        // 通过以上5步虽然可以解决焦点混乱的问题，但是在EditText的Text内容为空时
-        // 当前获取到焦点的EditText不一定能显示光标。
-        // 确保只有获取到焦点的view的CursorVisible为true。
-        holder.etLine.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus) {
-                    holder.etLine.setCursorVisible(true);
-                } else {
-                    holder.etLine.setCursorVisible(false);
-                }
-            }
-        });
         return convertView;
     }
 
